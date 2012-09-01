86965c07a7a43e4b7b99fb8e9e1fe4a8ad667c0d
fdas <info@fdas.com.ar>
2012-09-01 01:22:23 -0300

    stock, primeros cambios

---
+++ Stock_central_inicialdeejemplo.java
@@ -0,0 +1,18 @@
+
+
+/**
+ * @author Pavilion 6409
+ * @version 1.0
+ * @created 31-ago-2012 11:13:34 p.m.
+ */
+public class Stock_central {
+
+	public Stock_central(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+}
